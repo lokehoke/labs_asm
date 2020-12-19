57593bc59442ce14795c3e0dd6a1be1c4b75c1f4
lokehoke <ibymif2000@yandex.ru>
2020-12-19 22:55:29 +0300

    4

--- 7/4.cpp
+++ 7/4.cpp
@@ -1,104 +1,60 @@
-#include <iostream> 
-#include <ctime> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-using namespace std; 
-struct doubleArray 
-{ 
-int key; 
-double value; 
-}; 
-int main() 
-{ 
-srand(time(0)); 
-int N; 
-cout << "Enter size of structs N:"<< endl; 
-cin >> N; 
-struct doubleArray * Array=new struct doubleArray[N] ; 
-for (int i=0;i<N;i++) 
-{ 
-Array[i].key=rand()%(N/2); 
-Array[i].value=(rand()%1000)/(double)100; 
-cout << Array[i].key << "/" << Array[i].value << " "; 
-} 
-cout << "\nSizeof(Array[i]):" << sizeof(Array[0]) << "\n"; 
-cout << "\nSizeof(Array[i].key):" << sizeof(Array[0].key)<< endl; 
-cout << "\nSizeof(Array[i].value):" << sizeof(Array[0].value)<< endl; 
-cout << "key address:" << &Array[0].key << " value address:" << &Array[0].value; 
-cout << "\nEnter key:\n"; 
-int key = 0; 
-cin >> key; 
-double Min=-1; 
-asm ( 
-"push %%eax\n\t" 
-"push %%ebx\n\t" 
-"push %%ecx\n\t" 
-"movl $0, %%eax\n\t" 
-"Start:\n\t" 
-"cmpl %[N], %%eax\n\t" 
-"je ExitNM\n\t" 
-"movl %[Array], %%ecx\n\t" 
-"leal (%%ecx, %%eax, 8), %%ecx\n\t" 
-"leal (%%ecx, %%eax, 8), %%ecx\n\t" 
-"movl (%%ecx),%%ecx\n\t" 
-"cmpl %[Key], %%ecx\n\t" 
-"je lable\n\t" 
-"incl %%eax\n\t" 
-"jmp Start\n\t" 
-"lable:\n\t" 
-"movl %[Array], %%ecx\n\t" 
-"leal (%%ecx, %%eax, 8), %%ecx\n\t" 
-"leal 8(%%ecx, %%eax, 8), %%ecx\n\t" 
-//"movl (%%ecx),%%ecx\n\t" 
-//"movl %%ecx, %[Min]\n\t" 
-//"fldl %[Min]\n\t" 
-"fldl (%%ecx)\n\t" 
-"incl %%eax\n\t" 
-//-----------------------— 
-"StartSearchMin:\n\t" 
-"cmpl %[N], %%eax\n\t" 
-"je ExitHM\n\t" 
-"movl %[Array], %%ecx\n\t" 
-"leal (%%ecx, %%eax, 8), %%ecx\n\t" 
-"leal (%%ecx, %%eax, 8), %%ecx\n\t" 
-"movl (%%ecx),%%ecx\n\t" 
-"cmpl %[Key], %%ecx\n\t" 
-"jne CONTINUESEARCH\n\t" 
-"movl %[Array], %%ecx\n\t" 
-"leal (%%ecx, %%eax, 8), %%ecx\n\t" 
-"leal 8(%%ecx, %%eax, 8), %%ecx\n\t" 
-//"movl (%%ecx),%%ecx\n\t" 
-"fldl (%%ecx)\n\t" 
-"push %%ax\n\t" 
-"fcom\n\t" 
-"fnstsw\n\t" 
-//"fstpl %[Min]\n\t" 
-"sahf\n\t" 
-"pop %%ax\n\t" 
-"jc CHANGEMIN\n\t" 
-"fstpl %[Min]\n\t" 
-"jmp CONTINUESEARCH\n\t" 
-"CHANGEMIN:\n\t" 
-"fxch\n\t" 
-"fstpl %[Min]\n\t" 
-"CONTINUESEARCH:\n\t" 
-"incl %%eax\n\t" 
-"jmp StartSearchMin\n\t" 
-"ExitHM:\n\t" 
-"fstpl %[Min]\n\t" 
-"ExitNM:\n\t" 
-"pop %%ecx\n\t" 
-"pop %%ebx\n\t" 
-"pop %%eax\n\t" 
-: [Min]"+m" (Min) 
-: [Array]"m"(Array), [N]"m"(N), [Key]"m"(key) 
-: "cc" 
-); 
-if(Min==-1) 
-cout << "\nNo key = " << key << " elements\n"; 
-else 
-cout << "\nMinimum:" << Min<< endl; 
-delete [] Array; 
-return 0; 
+#include <iostream>
+
+struct KeyValuePair {
+    int key;
+    int value;
+};
+
+int main() {
+    srand(100);
+    int N = 15;
+    struct KeyValuePair * array = new struct KeyValuePair[N];
+
+    for (int i = 0 ; i < N ; i++) {
+        array[i].key=rand()%5;
+        array[i].value=rand()%(100)+1;
+        printf("Key = %d, Value = %d\n",array[i].key,array[i].value);
+    }
+
+    int key = 4;
+    int min = 0;
+    int max = 1000;
+
+    asm (R"(
+        movl $1, %%eax
+        movl %[max], %%edx
+        movl $0, %%eax
+        Start:
+        cmpl %[N], %%eax
+        je Exit
+        movl %[array], %%ecx
+        leal (%%ecx, %%eax, 8), %%ecx
+        movl (%%ecx),%%ecx
+        cmpl %[key],%%ecx
+        jne isOurKey
+        movl %[array], %%ecx
+        leal 4(%%ecx, %%eax, 8), %%ecx
+        movl (%%ecx),%%ecx
+        cmpl %%edx,%%ecx
+        ja isOurKey
+        movl %%ecx,%%edx
+        isOurKey: 
+        incl %%eax
+        jmp Start
+        Exit:
+        movl %%edx, %[min]
+    )"
+        : [min]"=m" (min)
+        : [array]"rm" (array), [N]"m"(N),[key]"m"(key),[max]"m"(max)
+        : "cc","%eax","%edx","%ecx"
+    );
+
+    if (min == max) {
+        std::cout << "No such elements:" << key << std::endl;
+    } else {
+        std::cout << "MIN (with key "<< key << ") = " << min << std::endl;
+    }
+
+    delete [] array;
+    return 0;
 }
--- 7/README.md
+++ 7/README.md
@@ -138,6 +138,73 @@ int main() {
 # Задание 4:
 ![](img/task4.png)
 
+## Программа на языке с++:
+````cpp
+#include <iostream>
+
+struct KeyValuePair {
+    int key;
+    int value;
+};
+
+int main() {
+    srand(100);
+    int N = 15;
+    struct KeyValuePair * array = new struct KeyValuePair[N];
+
+    for (int i = 0 ; i < N ; i++) {
+        array[i].key=rand()%5;
+        array[i].value=rand()%(100)+1;
+        printf("Key = %d, Value = %d\n",array[i].key,array[i].value);
+    }
+
+    int key = 4;
+    int min = 0;
+    int max = 1000;
+
+    asm (R"(
+        movl $1, %%eax
+        movl %[max], %%edx
+        movl $0, %%eax
+        Start:
+        cmpl %[N], %%eax
+        je Exit
+        movl %[array], %%ecx
+        leal (%%ecx, %%eax, 8), %%ecx
+        movl (%%ecx),%%ecx
+        cmpl %[key],%%ecx
+        jne isOurKey
+        movl %[array], %%ecx
+        leal 4(%%ecx, %%eax, 8), %%ecx
+        movl (%%ecx),%%ecx
+        cmpl %%edx,%%ecx
+        ja isOurKey
+        movl %%ecx,%%edx
+        isOurKey: 
+        incl %%eax
+        jmp Start
+        Exit:
+        movl %%edx, %[min]
+    )"
+        : [min]"=m" (min)
+        : [array]"rm" (array), [N]"m"(N),[key]"m"(key),[max]"m"(max)
+        : "cc","%eax","%edx","%ecx"
+    );
+
+    if (min == max) {
+        std::cout << "No such elements:" << key << std::endl;
+    } else {
+        std::cout << "MIN (with key "<< key << ") = " << min << std::endl;
+    }
+
+    delete [] array;
+    return 0;
+}
+````
+
+## Резальтут вызова:
+![](img/result4.png)
+
 
 # Задание 5:
 ![](img/task5.png)
